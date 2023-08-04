31092c5844cce0be337ec4a943e34667c0634893
vickrepe <85987845+vickrepe@users.noreply.github.com>
2023-08-04 17:46:31 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 <?php include 'head.php'; ?>
 <footer>
     <div class="contact-info">
-        <p>Teléfono: 123456789</p>
-        <p>Correo electrónico: info@ejemplo.com</p>
     </div>
     <div class="social-media">
         <a href="#"><i class="fa fa-facebook"></i></a>
